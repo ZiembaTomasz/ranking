a9816c6e2f436b5b37eef2437630b6ce6e61301f
ziembatomasz <ziembatomasz@gmail.com>
2020-04-01 21:06:11 +0100

    getRangeWithScores test

--- src/test/java/com/ziembatomasz/ranking/service/RankingServiceTest.java
+++ src/test/java/com/ziembatomasz/ranking/service/RankingServiceTest.java
@@ -3,23 +3,20 @@ package com.ziembatomasz.ranking.service;
 import com.ziembatomasz.ranking.dto.Player;
 import com.ziembatomasz.ranking.mapper.PlayerMapper;
 import com.ziembatomasz.ranking.repository.PlayerRepository;
-import org.junit.BeforeClass;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-
-
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ZSetOperations;
 import org.springframework.data.redis.core.ZSetOperations.TypedTuple;
 
-
 import java.util.HashSet;
 import java.util.Set;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -81,5 +78,26 @@ public class RankingServiceTest {
         //When
         rankingService.addPlayerScoreToJump(userId, score);
         verify(playerRepository, times(1)).save(player);
+    }
+
+    @Test
+    public void shouldGetRangeByScores() {
+        //Given
+        Player player = new Player("George Ptac", "347", 11.02);
+        Long startPosition = 1L;
+        Long endPosition = 999L;
+        String key = "Long Jump";
+        TypedTuple typedTuple = mock(TypedTuple.class);
+        Set<TypedTuple<String>> tulpedSet = new HashSet<>();
+        tulpedSet.add(typedTuple);
+        Set<Player> myPlayers = new HashSet<>();
+        myPlayers.add(player);
+        when(redisTemplate.opsForZSet()).thenReturn(zSetOperations);
+        when(zSetOperations.rangeByScoreWithScores(key, startPosition, endPosition)).thenReturn(tulpedSet);
+        when(playerMapper.mapToPlayer(tulpedSet)).thenReturn(myPlayers);
+        //When
+        Set<Player> newSet = rankingService.getRangeByScores(startPosition, endPosition);
+        //Then
+        assertEquals(1, newSet.size());
     }
 }
